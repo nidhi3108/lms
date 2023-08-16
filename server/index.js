47a1ba334e50a9dc5cc20388d428c4750f96bca2
Nidhi Verma <nidhivr310@gmail.com>
2023-08-16 17:59:40 +0530

    teacher register make

--- server/index.js
+++ server/index.js
@@ -3,14 +3,16 @@ const app=express();
 
 const port=5000;
 const studentrouter=require('./router/studentrouter')
+const teacherrouter=require('./router/teacherrouter')
 const cors=require('cors')
 
 app.use(express.json())
 app.use(cors({
-    origin:['*']
+    origin:['https://localhost:3000']
 }))
 
 app.use('/student',studentrouter)
+app.use('/teacher',teacherrouter)
 
 app.listen(port,()=>{
     console.log(`server is running at ${port}`)
--- server/router/teacherrouter.js
+++ server/router/teacherrouter.js
@@ -1,6 +1,21 @@
+const express= require('express')
 const model= require("../connection")
 const router=express.Router();
 
+
+
+router.post('/register',(req,res)=>{
+    console.log(req.body);
+    new model(req.body).save()
+    .then((result)=>{
+        console.log("data save");
+        res.json(result);
+    })
+    .catch((err)=>{
+        console.log(err);
+        res.json(err)
+    })
+})
 
 
 
