36c449e510d5fde8234cb0fc554fec0f7112e217
Nidhi Verma <nidhivr310@gmail.com>
2023-09-24 17:28:19 +0530

    Update TeacherRegister.js
    teacher

--- client/src/components/Teacher/TeacherRegister.js
+++ client/src/components/Teacher/TeacherRegister.js
@@ -3,7 +3,9 @@ import Register from "../Commonpages/Register";
 const TeacherRegister = (props) => {
   return (
     <>
-    <Register title="Register Here" name="Full Name" email="Email"  phone="Phone No" address="Address" interest="interest" qualification="Qualification" password="Password"/>
+    <Register title="Register Here" name="Full Name" email="Email"  phone="Phone No" address="Address" interest="interest" qualification="Qualification" password="Password"
+flag=="teacher"
+/>
     </>
   )
 }
